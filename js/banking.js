f7ce14202a3fb326b289f74a68a4773e29800fd5
Salman <you@example.com>
2022-02-18 11:22:11 +0600

    balance new function

--- js/banking.js
+++ js/banking.js
@@ -15,12 +15,22 @@ function updaateTotal(fildId, amount) {
     const previousTotal = getInnerTextValue(fildId);
     const newTotal = previousTotal + amount;
     document.getElementById(fildId).innerText = newTotal
+    return newTotal
+}
+
+function updateBalance() {
+    const previousBalance = getInnerTextValue('balance-total');
+    const diposit = getInnerTextValue('diposit-total');
+    const withdraw = getInnerTextValue('withdraw-total');
+    const newBalance = (diposit - withdraw) + previousBalance;
+    document.getElementById('balance-total').innerText = newBalance
 }
 
 document.getElementById('diposit-btn').addEventListener('click', function () {
     const amount = getInputValue('diposit-input');
     if (amount > 0) {
         updaateTotal('diposit-total', amount)
+        updateBalance()
     }
 })
 
@@ -28,5 +38,6 @@ document.getElementById('withdraw-btn').addEventListener('click', function () {
     const amount = getInputValue('withdraw-input');
     if (amount > 0) {
         updaateTotal('withdraw-total', amount)
+        updateBalance()
     }
 })
